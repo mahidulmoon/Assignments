1c2de74f335bae22282506cb429b47383a55c99c
mahidulmoon <mahidulmoon@gmail.com>
2020-09-23 15:09:09 +0600

    user login and registration success

---
+++ assignment_test/user/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields=['id','username', 'password','email']
+        #extra_kwargs = {'password': {'write_only':True, 'required':True}}
+
+
+    def create(self,validated_data):
+        user = User.objects.create_user(**validated_data)
+        user.save()
+        Token.objects.create(user=user)
+        return user
---
+++ assignment_test/user/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path,include
+from rest_framework import routers
+from .views import UserViewSet, CustomObtainAuthToken
+
+
+router = routers.DefaultRouter()
+router.register('registration',UserViewSet)
+
+
+urlpatterns = [
+    path('',include(router.urls)),
+    path('authenticate/', CustomObtainAuthToken.as_view()),
+]
