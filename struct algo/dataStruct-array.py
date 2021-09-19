0c5b7cb5df7384a984692273df2377ac58ced76c
Mahidul Moon <49182474+mahidulmoon@users.noreply.github.com>
2021-09-19 21:55:31 +0600

    traversing an array

--- struct algo/dataStruct-array.py
+++ struct algo/dataStruct-array.py
@@ -1,4 +1,12 @@
-# creating an 1D array
+### creating an 1D array
 
 array = [x for x in range(0,11)]
+#[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(array)
+
+
+
+###traversing 1D array
+
+for i in range(len(array)):
+    print(i,end=" ")
