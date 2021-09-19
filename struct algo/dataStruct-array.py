2ce7e00cc7a4b5c19df01f6d5a77685f4a850168
Mahidul Moon <49182474+mahidulmoon@users.noreply.github.com>
2021-09-19 22:53:14 +0600

    basics of array

--- struct algo/dataStruct-array.py
+++ struct algo/dataStruct-array.py
@@ -10,3 +10,22 @@ print(array)
 
 for i in range(len(array)):
     print(i,end=" ")
+
+print("")
+###get from array 
+def getArr(arr,index):
+    return arr[index]
+
+print(getArr(array,-1))
+
+### inserting into an index number without built in function
+def insertArr(arr,value,index):
+    arr[index] = value
+    return arr
+print(insertArr(array,5,2))
+
+def delArr(arr,index):
+    arr[index] = 0
+    return arr
+
+print(delArr(array,5))
