6e0593a3c4980a8b8494c7578620ee745a95d93c
Mahidul Moon <49182474+mahidulmoon@users.noreply.github.com>
2021-09-20 23:44:06 +0600

    Update array2D.py

--- struct algo/array2D.py
+++ struct algo/array2D.py
@@ -1,10 +1,18 @@
 array = [
     [1,2],
     [3,4],
-    [5,6]
+    [5,6],
 ]
 
 def getValue(arr,r,c):
     return arr[r][c]
-
+#getting value
 print(getValue(array,1,1))
+
+#adding value
+array.insert([3][0],[10,20])
+print(array)
+
+#updating value
+array[2][1] = 100
+print(array)
