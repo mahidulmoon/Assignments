1aa70f1d979f06a58d678192b655fe14853129d7
Mahidul Moon <49182474+mahidulmoon@users.noreply.github.com>
2021-09-22 00:30:39 +0600

    traversing

--- struct algo/array2D.py
+++ struct algo/array2D.py
@@ -16,3 +16,10 @@ print(array)
 #updating value
 array[2][1] = 100
 print(array)
+
+
+#2D array traversing
+print("2D array traversing")
+for i in range(len(array) -1):
+    for j in range(len(array[0])-1):
+        print(array[i][j])
